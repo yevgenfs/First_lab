1bb924c624e3adbc7d924225a5e531dbdae568e7
Yevhen Surcov <yevgenfs2@gmail.com>
2019-02-15 09:49:01 +0200

    Searching interface

---
+++ IFishSearchAndSort.java
@@ -0,0 +1,21 @@
+package manager;
+
+import java.util.List;
+
+import ua.lviv.IoT.fish.FishProducts;
+import ua.lviv.IoT.fish.KindOfFish;
+import ua.lviv.IoT.fish.QualityOfFish;
+
+public interface IFishSearchAndSort {
+
+	public List<FishProducts> priceAndKindOfSort( List<FishProducts> fishList, int hight,int low,KindOfFish kindOfFish);
+	
+	public List<FishProducts> nameIncrese ( List<FishProducts> fish);
+	
+	public  List<FishProducts> nameDecrese (List<FishProducts> fish);
+	
+	public List<FishProducts> weightDecrese( List<FishProducts> fish);
+	
+	public  List<FishProducts> weightIncrese(List<FishProducts> fish);
+	
+}
