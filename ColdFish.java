ba8a8c353aa25a0cb23fb4baeefe744c55dcc43a
Yevhen Surcov <yevgenfs2@gmail.com>
2019-02-17 20:04:01 +0200

    cold fish class of first lab

--- ColdFish.java
+++ ColdFish.java
@@ -3,11 +3,7 @@ package ua.lviv.IoT.fish;
 public class ColdFish extends FishProducts {
 	 private int temperature;
 
-	public ColdFish(KindOfFish kindOfFish, int price, Import imports, boolean keepingStorageConditon,
-			String fromWhichCountry, QualityOfFish qualityOfFish, String nameOfwfsh, double weight, int temperature) {
-		super(kindOfFish, price, imports, keepingStorageConditon, fromWhichCountry, qualityOfFish, nameOfwfsh, weight);
-		this.temperature = temperature;
-	}
+	public ColdFish () {}
 
 	
 	
