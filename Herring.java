95105f70b5233878bbf85d4bdd1cccf26cfeb571
Yevhen Surcov <yevgenfs2@gmail.com>
2019-02-21 02:28:43 +0200

    Herring class for 4 lab

--- Herring.java
+++ Herring.java
@@ -1,29 +1,29 @@
-package ua.lviv.IoT.fish;
+package main.java.ua.lviv.IoT.fish;
 
 public class Herring extends FishProducts {
- private int experiationDate ;
- private int quantiti  ;
+  private int experiationDate;
+  private int quantiti;
  
 
- public Herring() {} 
+  public Herring() {} 
 
  
 
 
-public int getExperiationDate() {
-	return experiationDate;
-}
+  public int getExperiationDate() {
+    return experiationDate;
+  }
 
-public void setExperiationDate(int experiationDate) {
-	this.experiationDate = experiationDate;
-}
+  public void setExperiationDate(int experiationDate) {
+    this.experiationDate = experiationDate;
+  }
 
-public int getQuantiti() {
-	return quantiti;
-}
+  public int getQuantiti() {
+    return quantiti;
+  }
 
-public void setQuantiti(int quantiti) {
-	this.quantiti = quantiti;
-}
+  public void setQuantiti(int quantiti) {
+    this.quantiti = quantiti;
+  }
  
 }
