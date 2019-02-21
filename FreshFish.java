4ccb886d1c8eae9d417ef43d4e33201015510a42
Yevhen Surcov <yevgenfs2@gmail.com>
2019-02-21 02:30:34 +0200

    FreshFish  class for 4 lab

--- FreshFish.java
+++ FreshFish.java
@@ -1,44 +1,36 @@
-package ua.lviv.IoT.fish;
+package main.java.ua.lviv.IoT.fish;
 
 public class FreshFish extends FishProducts {
-	
-	private int experiationDate ;
-	private InWhichStatus inWhichStatus;
 
-	public FreshFish( ) {}
-		
-	
-	
-	
-	public void cut ()
-	{
-		 if(inWhichStatus ==  InWhichStatus.CUTED)
-		 {
-		  System.out.println ("It was Cuted before");
-		 }
-		 
-		 else {
-	      inWhichStatus =  InWhichStatus.CUTED;
-		 }
+  private int experiationDate;
+  private InWhichStatus inWhichStatus;
+
+  public FreshFish() {}
+
+  public void cut() {
+    if (inWhichStatus == InWhichStatus.CUTED) {
+      System.out.println("It was Cuted before"); 
+    } else {
+      inWhichStatus = InWhichStatus.CUTED;
+    }
  
-	}
+  }
 
-	public int getExperiationDate() {
-		return experiationDate;
-	}
+  public int getExperiationDate() {
+    return experiationDate;
+  }
 
-	public void setExperiationDate(int experiationDate) {
-		this.experiationDate = experiationDate;
-	}
+  public void setExperiationDate(int experiationDate) {
+    this.experiationDate = experiationDate;
+  }
 
-	public InWhichStatus getInWhichStatus() {
-		return inWhichStatus;
-	}
+  public InWhichStatus getInWhichStatus() {
+    return inWhichStatus;
+  }
 
-	public void setInWhichStatus(InWhichStatus inWhichStatus) {
-		this.inWhichStatus = inWhichStatus;
-	}
-	
-	
+  public void setInWhichStatus(InWhichStatus inWhichStatus) {
+    this.inWhichStatus = inWhichStatus;
+  }
+
 
 }
