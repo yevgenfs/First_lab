9e1ef21316bfee1030e3228b1c5630e6635baacc
Yevhen Surcov <yevgenfs2@gmail.com>
2019-03-01 20:35:23 +0200

    main class of six lab

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 package ua.lviv.IoT.fish;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
 import java.util.List;
 import ua.lviv.IoT.fish.FishProducts;
+import ua.lviv.IoT.fish.FishProductsWriter;
 import ua.lviv.IoT.fish.KindOfFish;
 import ua.lviv.IoT.fish.Import;
 import ua.lviv.IoT.fish.QualityOfFish;
@@ -10,8 +13,8 @@ import ua.lviv.IoT.fish.QualityOfFish;
 
 public class Main {
 
-  public static void main(String[] args) {
-   /* FishProducts fish1 = new  FishProducts(KindOfFish.FRESHFISH,15,Import.IMPORT,true,"England",
+  public static void main(String[] args) throws IOException {
+    FishProducts fish1 = new  FishProducts(KindOfFish.FRESHFISH,15,Import.IMPORT,true,"England",
         QualityOfFish.WELLQUALITY,"a",54);
     FishProducts fish2 = new  FishProducts(KindOfFish.FRESHFISH,20,Import.IMPORT,true,"England",
         QualityOfFish.WORSTQUALITY,"b",48);
@@ -27,8 +30,8 @@ public class Main {
     fishList.add(fish3);
     fishList.add(fish4);
     fishList.add(fish5);
-
-    for (int i = 0;i < fishList.size();i++) {
+    fishList.get(0).writeToFile(fishList);
+    /*for (int i = 0;i < fishList.size();i++) {
 
       fishList.get(i).priceAndKindOfSort(fishList,21,10,KindOfFish.FRESHFISH);
       System.out.println(fishList.get(i).toString());
